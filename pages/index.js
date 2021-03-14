ed642bef6c7eca5526d7b27b3cf9caa89d572e65
Jamie Holliday <jamie.holliday@myjobmatcher.com>
2021-03-14 12:54:55 +0000

    setup

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Baldy and Blondey</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
+    </div>
+  );
+}
